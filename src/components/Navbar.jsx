fc6da49a3b18fa723b7f4063e4d5d26063710c5c
Raghu-KV <raghunandanv19@gmail.com>
2023-04-04 17:25:35 +0530

    4.4.2023 of

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { HiOutlineMenuAlt3, HiX, HiLogout } from "react-icons/hi";
 import { useState } from "react";
 
 function Navbar() {
@@ -15,32 +15,61 @@ function Navbar() {
 
   return (
     <>
-      <div className="bg-slate-700 relative">
-        <div className="h-16  flex items-center justify-between container mx-auto">
+      <div className="bg-slate-700 fixed w-screen">
+        <div className="h-16 flex items-center justify-between container mx-auto">
           <div className="ml-5 md:ml-0">
             <h1 className="text-white font-bold text-2xl">
               <Link to="/">URL shortner</Link>
             </h1>
           </div>
           <ul className="hidden text-white md:flex gap-4 font-semibold">
-            <li>
+            <li className="py-2">
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li className="py-2">
+              {" "}
+              <Link to="/statistics">Statistics</Link>
+            </li>
+            <li
+              onClick={logOut}
+              className="cursor-pointer rounded-lg px-6 py-2 bg-slate-900 hover:scale-105 transition-all duration-100"
+            >
+              <HiLogout className="inline-block mr-2 mb-1" />
+              Logout
+            </li>
+          </ul>
+          <div
+            className="mr-5 text-white md:hidden z-10"
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <HiX size={"30px"} />
+            ) : (
+              <HiOutlineMenuAlt3 size={"30px"} />
+            )}
+          </div>
+
+          <ul
+            className={`h-screen absolute w-4/6 bg-slate-700  right-0 top-0 ${
+              openNav ? "translate-x-0" : "translate-x-full"
+            } transition-all ease-out duration-300 text-white flex flex-col gap-2 font-semibold items-center justify-center
+            text-xl`}
+          >
+            <li className="">
               <Link to="/">Dashboard</Link>
             </li>
             <li>
               {" "}
               <Link to="/statistics">Statistics</Link>
             </li>
-            <li onClick={logOut} className="cursor-pointer">
+            <li
+              onClick={logOut}
+              className="cursor-pointer mt-10 rounded-lg px-6 py-2 bg-slate-900"
+            >
+              <HiLogout className="inline-block mr-2 mb-1" />
               Logout
             </li>
           </ul>
-          <div className="mr-5 text-white md:hidden z-10">
-            <HiOutlineMenuAlt3 size={"30px"} />
-          </div>
-
-          <div
-            className={`h-screen w-3/4 bg-blue-500 absolute right-0 top-0`}
-          ></div>
         </div>
       </div>
       <Outlet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 function Home() {
   const [userData, setUserData] = useState("");
@@ -25,7 +27,63 @@ function Home() {
     getUserData();
   }, []);
 
-  return <h1>this is home</h1>;
+  // console.log(userData);
+
+  const formik = useFormik({
+    initialValues: {
+      url: "asdasd",
+    },
+    validationSchema: yup.object({
+      url: yup
+        .string()
+        .matches(
+          /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+          "Enter valid url!"
+        )
+        .required("Please enter a Link!"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className="container mx-auto pt-20">
+      <h1 className="text-white text-center text-3xl font-semibold">
+        Hi {userData.userName}
+      </h1>
+
+      <div className="mt-6 p-3">
+        <form
+          className="flex justify-center gap-2 text-white"
+          onSubmit={formik.handleSubmit}
+        >
+          <input
+            type="text"
+            className={`w-full px-3 bg-transparent border-b-2 focus:outline-none focus:border-b-green-600  ${
+              formik.touched.url && formik.errors.url
+                ? "border-b-red-600"
+                : "border-b-white"
+            }`}
+            name="url"
+            value={formik.values.url}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+
+          <button
+            type="submit"
+            className=" px-6 py-3 font-semibold bg-green-600 rounded-lg hover:scale-105 transition-all duration-100 "
+          >
+            Shrink
+          </button>
+        </form>
+        {formik.touched.url && formik.errors.url && (
+          <p className="text-red-500 pt-2 font-semibold">{formik.errors.url}</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
