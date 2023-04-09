9f259f70ea7d7c9b90edb549d0a4e208bac672b7
Raghu-KV <raghunandanv19@gmail.com>
2023-04-09 15:04:37 +0530

    local storage

--- src/Context.js
+++ src/Context.js
@@ -1,14 +1,35 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { URL } from "../src/BackEndURL";
 
 export const allDataContext = createContext();
 
 export function DataProvider({ children }) {
   const [userData, setUserData] = useState("");
+  const navigate = useNavigate();
+
+  const getUserData = async () => {
+    const localStorageToken = localStorage.getItem("token");
+
+    const responce = await fetch(`${URL}/users/who-has-logged-in`, {
+      method: "GET",
+      headers: { "x-auth-token": localStorageToken },
+    });
+    if (responce.status === 401) {
+      navigate("/log-in");
+    } else {
+      const data = await responce.json();
+      setUserData(data);
+    }
+  };
 
   return (
     <allDataContext.Provider
-      value={{ userData: userData, setUserData: setUserData }}
+      value={{
+        userData: userData,
+        setUserData: setUserData,
+        getUserData: getUserData,
+      }}
     >
       {children}
     </allDataContext.Provider>
